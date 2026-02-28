d5cea9838f20724f4b689e1baf082cab4adb7e49
shuiyigong0210-byte <shuiyigong0210@gmail.com>
2026-02-28 20:02:32 +0000

    3

--- path_2.gd
+++ path_2.gd
@@ -6,14 +6,14 @@ func _ready() -> void:
 	
 	# 如果当前不是服务器（也就是 Join 的人）
 	if not multiplayer.is_server():
+		self.visible = true
+		print("服务端：检测到身份为 Join，保留路径引导图")
+	else:
 		# 彻底隐藏并禁用处理
 		self.visible = false
 		# 如果你担心它还是会闪现，直接在本地物理删除它
 		self.queue_free()
-		print("客户端：检测到身份为 Join，已移除路径引导图")
-	else:
-		self.visible = true
-		print("服务端：检测到身份为 Host，保留路径引导图")
-
+		print("客户端：检测到身份为 Host，已移除路径引导图")
+		
 func _process(_delta: float) -> void:
 	pass
